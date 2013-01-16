8929e8c7994184eed465f7518118f46f97efaaeb
Jeroen Keiren <jkeiren@gmail.com>
2013-01-16 17:05:15 +0100

    Allow control over long-running operations

--- src/pginfo.cpp
+++ src/pginfo.cpp
@@ -14,7 +14,8 @@ private:
   std::ofstream m_ofstream;
 
 protected:
-  bool m_expensive;
+  size_t m_max_vertices_for_expensive;
+  size_t m_neighbourhood_upto;
   typedef tools::input_output_tool super;
 
 public:
@@ -23,7 +24,8 @@ public:
                                         "Jeroen J.A. Keiren",
                                         "Provides various sorts of structural information about parity games.",
                                         "Structural properties that are described in the paper XXX"), // TODO
-      m_expensive(false)
+      m_max_vertices_for_expensive(1000),
+      m_neighbourhood_upto(3)
   {}
 
   void
@@ -31,13 +33,23 @@ public:
   {
     super::add_options(desc);
     desc.
-    add_option("expensive", "perform expensive checks");
+        add_option("max-vertices-for-expensive", make_mandatory_argument("NUM"),
+                   "perform expensive checks only if the number of vertices is at most NUM", 'm').
+        add_option("neighbourhood-upto", make_mandatory_argument("NUM"),
+                   "compute all neighbourhoods up to and including the NUM-neighbourhood", 'n');;
   }
 
   void parse_options(const command_line_parser& parser)
   {
     super::parse_options(parser);
-    m_expensive = parser.options.count("expensive");
+    if (parser.options.count("max-vertices-for-expensive"))
+    {
+      m_max_vertices_for_expensive = parser.option_argument_as< size_t > ("max-vertices-for-expensive");
+    }
+    if (parser.options.count("neighbourhood-upto"))
+    {
+      m_neighbourhood_upto = parser.option_argument_as< size_t > ("neighbourhood-upto");
+    }
   }
 
   bool run()
@@ -49,7 +61,7 @@ public:
     parse_pgsolver(pg, is, timer());
     YAML::Emitter out;
 
-    report(pg, out);
+    report(pg, out, m_max_vertices_for_expensive, m_neighbourhood_upto);
 
     os << out.c_str() << std::endl;
 
